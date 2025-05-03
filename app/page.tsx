90ea8512f5ded34e155366c9f07535e1b97c4d62
03_junghee <03_junghee@naver.com>
2025-05-03 13:49:35 +0900

    first modify

---
+++ app/page.tsx
@@ -0,0 +1,11 @@
+// app/page.tsx
+
+import styles from './styles/Home.module.css';
+
+export default function Home() {
+  return (
+    <main className={styles.container}>
+      <h1 className={styles.title}>BRIEFIN</h1>
+    </main>
+  );
+}
